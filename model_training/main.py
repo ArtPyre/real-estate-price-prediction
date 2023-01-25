d6c6c3f2ee54fc6a1b3ee4d017e30bf7aa1cd3da
Arthur Pyre <arthurpyre@hotmail.com>
2023-01-25 16:46:23 +0100

    Model training part 2

--- model_training/main.py
+++ model_training/main.py
@@ -5,22 +5,19 @@ import models
 path = os.path.join(os.path.abspath('data_acquisition'), 'all_infos_processed.csv')
 df = pd.read_csv(path)
 df = df.dropna(subset=['Price','Living_Area'])
-print(models.random_forest_model(df['Price'].array, df['Living_Area'].array))
-print(models.linear_regression_model(df['Price'].array, df['Living_Area'].array))
 
-'''X=df.drop('Price',axis=1) 
+print("Random forest score : {}".format(models.random_forest_model(df['Price'].array, df['Living_Area'].array)))
+print("Linear regression score : {}".format(models.linear_regression_model(df['Price'].array, df['Living_Area'].array)))
+
+X=df.drop('Price',axis=1)
 y=df['Price']
 
 formated_columns = []
-
 for column_name in X.columns :
     if(X[column_name].dtype == 'object') :
-        print("column {} has been formated".format(column_name))
         formated_columns.append(column_name)
 X = pd.get_dummies(df, prefix=formated_columns, columns=formated_columns)
 
-print(X.shape)
-print(X.head())'''
 
 
 
--- model_training/models.py
+++ model_training/models.py
@@ -1,19 +1,39 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 def random_forest_model (X, y) :
-    X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=41, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
+    X_train = X_train.reshape(-1, 1)
+    X_test = X_test.reshape(-1, 1)
 
     regression = RandomForestRegressor()
-    regression.fit(X_train.reshape(-1, 1),y_train)
-    return regression.score(X_test.reshape(-1, 1), y_test)
+    regression.fit(X_train,y_train)
+
+    regression.predict(X_test)
+
+    return regression.score(X_test, y_test)
 
 
 def linear_regression_model (X, y) :
-    X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=41, test_size=0.2)  
-
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)  
+    X_train = X_train.reshape(-1, 1)
+    X_test = X_test.reshape(-1, 1)
     regression = LinearRegression()
-    regression.fit(X_train.reshape(-1, 1), y_train)
-    return regression.score(X_test.reshape(-1, 1), y_test)
+    regression.fit(X_train, y_train)
+
+    plt.scatter(X_train, y_train)
+    plt.plot(X_train,regression.predict(X_train), color='red')
+    plt.title("Living_Area VS Price (Training set)")
+    plt.ylabel("Living_Area")
+    plt.xlabel("Price")
+    plt.xlim(0,1001000)
+    plt.ylim(0,400)
+    plt.savefig("./model_training/graphs/Living_Arae_VS_Price_Linear.png")
+
+    regression.predict(X_test)
+    
+    return regression.score(X_test, y_test)
+
 
